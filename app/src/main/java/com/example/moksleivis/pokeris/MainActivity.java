cf0ed7aa86dbe1c537852dc41767b7e9ecbe8d06
mantas1985 <mantasnaudzius@gmail.com>
2017-03-30 15:28:56 +0300

    Slaptazodzio ir vartotojo vardo ivedimas ir tikrinimas

--- app/src/main/java/com/example/moksleivis/pokeris/MainActivity.java
+++ app/src/main/java/com/example/moksleivis/pokeris/MainActivity.java
@@ -5,13 +5,30 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static android.R.string.cancel;
 
 public class MainActivity extends AppCompatActivity {
-
+private EditText mUsernameView;
+private EditText mPasswordView;
+private View focusView = null;
+    private    String username;
+    private    String password;
+    private boolean cancel = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mUsernameView = (EditText) findViewById(R.id.prisijungimoVardas);
+        mPasswordView = (EditText) findViewById(R.id.slaptazodis);
+        mUsernameView.setError(null);
+        mPasswordView.setError(null);
 
         Button mygtukasPrisijungimas = (Button) findViewById(R.id.prisijungti);
         mygtukasPrisijungimas.setOnClickListener(new View.OnClickListener() {
@@ -33,13 +50,43 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-    public void prisijungimas2(){
-        Intent intent = new Intent (MainActivity.this, prisijungimas.class);
-        startActivity(intent);
-    }
+    public void prisijungimas2() {
 
+        username =mUsernameView.getText().toString();
+        password =mPasswordView.getText().toString();
+
+        Toast.makeText(MainActivity.this,username + "" + password,
+                Toast.LENGTH_SHORT).show();
+
+        if (!isValid(username)) {
+            mUsernameView.setError(getString(R.string.error_invalid_username));
+            focusView = mUsernameView;
+            cancel = true;
+        }
+        if (!isValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+        if (cancel) {
+            focusView.requestFocus();
+        } else {
+
+
+            Intent intent = new Intent(MainActivity.this, prisijungimas.class);
+            startActivity(intent);
+        }
+    }
     public void registracija2(){
         Intent intent = new Intent (MainActivity.this, registracija.class);
         startActivity(intent);
+    }
+    private boolean isValid(String credentials){
+
+        final String CREDENTIALS_PATTERN = "^([0-9a-z]{3,15})+$";
+        Pattern pattern = Pattern.compile(CREDENTIALS_PATTERN);
+
+        Matcher matcher = pattern.matcher(credentials);
+        return matcher.matches();
     }
 }
